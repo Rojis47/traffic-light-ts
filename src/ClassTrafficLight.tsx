6626b7ef5664245b587e61aad9da51d29b0d1348
Rogelio Servin <manchaster1509@gmail.com>
2023-08-27 17:36:43 -0500

    initial commit

--- src/ClassTrafficLight.tsx
+++ src/ClassTrafficLight.tsx
@@ -1,17 +1,62 @@
 import { Component } from "react";
 
-export class ClassTrafficLight extends Component {
+interface LightBarProps {
+  color?: string;
+  index?: number;
+  lightIndex?: number;
+}
+
+class LightBar extends Component<LightBarProps> {
+  static defaultProps = {
+    color: "black",
+    index: -1,
+    lightIndex: -1,
+  };
+
   render() {
+    const { color, index, lightIndex } = this.props;
+    return lightIndex === index ? (
+      <div className={`${color} circle`}></div>
+    ) : null;
+  }
+}
+
+interface ClassTrafficLightState {
+  lightIndex: number;
+}
+
+export class ClassTrafficLight extends Component<
+  Record<string, never>,
+  ClassTrafficLightState
+> {
+  constructor(props: Record<string, never>) {
+    super(props);
+    this.state = {
+      lightIndex: 0,
+    };
+  }
+
+  handleNextState = () => {
+    this.setState((prevState) => ({
+      lightIndex: (prevState.lightIndex + 1) % 3,
+    }));
+  };
+
+  render() {
+    const { lightIndex } = this.state;
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
-          {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          <LightBar color="red" index={2} lightIndex={lightIndex} />
+          <LightBar />
+          <LightBar color="yellow" index={1} lightIndex={lightIndex} />
+          <LightBar />
+          <LightBar color="green" index={0} lightIndex={lightIndex} />
         </div>
-        <button className="next-state-button">Next State</button>
+        <button onClick={this.handleNextState} className="next-state-button">
+          Next State
+        </button>
       </div>
     );
   }
--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -1,17 +1,40 @@
-export const FunctionalTrafficLight = () => {
+import React, { useState } from "react";
+
+interface LightBarProps {
+  color?: string;
+  index?: number;
+  lightIndex?: number;
+}
+
+function LightBar({
+  color = "black",
+  index = -1,
+  lightIndex = -1,
+}: LightBarProps) {
+  return lightIndex === index ? (
+    <div className={`${color} circle`}></div>
+  ) : null;
+}
+
+export const FunctionalTrafficLight: React.FC = () => {
+  const [lightIndex, setLightIndex] = useState<number>(0);
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
-        {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <LightBar color="red" index={2} lightIndex={lightIndex} />
+        <LightBar />
+        <LightBar color="yellow" index={1} lightIndex={lightIndex} />
+        <LightBar />
+        <LightBar color="green" index={0} lightIndex={lightIndex} />
       </div>
-      <button className="next-state-button">Next State</button>
+      <button
+        onClick={() => setLightIndex((lightIndex + 1) % 3)}
+        className="next-state-button"
+      >
+        Next State
+      </button>
     </div>
   );
 };
-
-
-
